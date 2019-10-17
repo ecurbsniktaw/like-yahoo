1cd956a96a8701132671bfbdd0f457b156107b05
Bruce Watkins <bruce.watkins@gmail.com>
2019-10-17 11:37:29 +0000

    Initial Page Formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Returns the full title for a page.
+  def full_title(page_title='')
+    base_title="Like Yahoo"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
